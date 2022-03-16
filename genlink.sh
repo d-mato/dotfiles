b02df147bd2b86b417dee14378b64d7da3352628
Daiki Matoba <telnetstat@gmail.com>
2022-03-16 16:41:29 +0900

    Update

--- genlink.sh
+++ genlink.sh
@@ -2,5 +2,6 @@
 
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/eslintrc ~/.eslintrc
-ln -sf $PWD/rubocop.yml ~/.rubocop.yml
+ln -sf $PWD/gitignore_global ~/.gitignore_global
+ln -sf $PWD/rufo ~/.rufo
+ln -sf $PWD/prettierrc.json ~/.prettierrc.json
