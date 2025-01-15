23902fb6caa7dc0a22d13d82a5fd200a1ab44a0c
Daiki Matoba <telnetstat@gmail.com>
2025-01-15 14:41:59 +0900

    Disable links

--- genlink.sh
+++ genlink.sh
@@ -2,6 +2,5 @@
 
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/gitignore_global ~/.gitignore_global
-ln -sf $PWD/rufo ~/.rufo
-ln -sf $PWD/prettierrc.json ~/.prettierrc.json
+# ln -sf $PWD/rufo ~/.rufo
+# ln -sf $PWD/prettierrc.json ~/.prettierrc.json
